fc9e0a0c606bcd6f015624025d411a7ab60008d1
A2ZH <thejianmail@gmail.com>
2016-03-14 20:41:42 +0800

    Intellisense completions

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="browser/ambient/express/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/react-global/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="main/ambient/express/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/ambient/react-global/index.d.ts" />
