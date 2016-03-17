b5c221c1bbdc9d16189e7cae789ee021d77b6fd9
A2ZH <thejianmail@gmail.com>
2016-03-17 22:36:29 +0800

    fixed typing mistake

--- comments-box/public/javascript/commentsBox.js
+++ comments-box/public/javascript/commentsBox.js
@@ -80,7 +80,7 @@ var CommentBox = React.createClass({
       type: 'POST',
       data: comment,
       success: function(data) {
-        this.setState({data});
+        this.setState({data: data});
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
