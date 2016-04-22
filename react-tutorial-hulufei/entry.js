b1910509189c8f3a5afe42ba08d773c301d97c45
theJian <thejianmail@gmail.com>
2016-04-22 13:13:43 +0800

    react component

---
+++ react-tutorial-hulufei/entry.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class LikeButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  handleClick(e) {
+    this.setState({ liked: !this.state.liked });
+  }
+
+  render() {
+    const text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      <p onClick={this.handleClick.bind(this)}>
+          You {text} this. Click to toggle.
+      </p>
+    );
+  }
+}
+
+render(
+    <LikeButton />,
+    document.getElementById('example')
+);
